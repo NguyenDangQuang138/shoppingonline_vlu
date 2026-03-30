d05fdb94f36f81df3aaa2a3ac7d48893871004a3
NguyenDangQuang138 <Caiquatmay138@gmail.com>
2026-03-30 23:01:58 +0700

    Add files via upload

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,14 @@
+const MyConstants = {
+  DB_SERVER: "cluster0.ikbcwsf.mongodb.net",
+  DB_USER: "NguyenDangQuang123",
+  DB_PASS: "Wd7giVbkjUE1rbad",
+  DB_DATABASE: "shoppingonline",
+
+  EMAIL_USER: "Caiquatmay138@gmail.com",
+  EMAIL_PASS: "jyxzmxpjcsusboip",
+
+  JWT_SECRET: "qU8#9@LsP!xZ_2026_Admin_Auth",
+  JWT_EXPIRES: "3562225566555245", // in milliseconds
+};
+
+module.exports = MyConstants;
